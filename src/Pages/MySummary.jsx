dcb790608f129d35a950cdf5e96dde6c7fb7db4f
seanlb02 <seangyuris1@gmail.com>
2023-02-03 14:24:49 +1100

    changes to friend page reload

--- src/Pages/MySummary.jsx
+++ src/Pages/MySummary.jsx
@@ -20,6 +20,7 @@ export default function UserSummary() {
 
     const userscores = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, {name: 'Page B', uv: 600, pv: 2400, amt: 2400},{name: 'Page B', uv: 800, pv: 2400, amt: 2400}]
     const [scoresArray, setScoresArray] = useState("");
+    const [categories, setCategoriesArray] = useState([{name: 'Balanced',name: 'Monitor', name: 'Seek advice'  }]);
 
     return(
         <>
@@ -32,6 +33,7 @@ export default function UserSummary() {
         <Line type="monotone" dataKey="score" stroke="#8884d8" />
  
         <XAxis  tick={false}/>
+ 
         <YAxis type="number" domain={[10,50]}/>
     </LineChart>
     </div>
