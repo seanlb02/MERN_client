d8431e87f7b4bff6330a4db839ae6e886f7e0eb5
seanlb02 <seangyuris1@gmail.com>
2023-02-04 16:41:30 +1100

    added

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
+  server: {
       port: process.env.PORT || "mernclient-production.up.railway.app"
-    },
+      },
     watch: {
       usePolling: true,
     }
   }
-})
+)
