13783202947cd4d68a9f6999c484f39601f172e1
seanlb02 <seangyuris1@gmail.com>
2023-02-04 11:44:00 +1100

    made navbar conditionally rendered

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, BrowserRouter, Routes, Link } from 'react-router-dom'
 import { Nav, Navbar, NavDropdown, Button } from "react-bootstrap"
 import { LogOut } from '../API services (fetch functions)/authServices';
-
+import {useState, useEffect} from 'react'
 
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -10,8 +10,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 
 export default function Navigator() {
+
+  useEffect(() => {   
+    if(localStorage.getItem("tokenKey") != null) {
+      setLoggedIn(true)
+    }
+},[]);
+
+
+  const [loggedIn, setLoggedIn] = useState(false)
+
+
+
   return (
     <>
+
+
+{ loggedIn ? 
+
     <nav className="navbar navbar-expand-lg navbar light">
         <a className="navbar-brand" href="#"><img src="src/assets/logo.png" alt= "logo"></img></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -39,9 +55,29 @@ export default function Navigator() {
             </li>
         </ul>
       </div>
-    </nav>
+    </nav> 
+
+    :
+
+    <nav className="navbar navbar-expand-lg navbar light ">
+                <a className="navbar-brand" href="#"><img src="src/assets/logo.png" alt="logo"></img></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ms-auto">
+                    <li className="nav-item active px-3">
+                        <a className="nav-link" href="/login">Login</a>
+                        </li>
+                    <li className="nav-link px-3">
+                        <a href="/register"><button className="btn btn-primary btn-lg round" type="submit">Sign-up</button></a>
+                    </li>
+                </ul>
+                </div>
+            </nav>
 
 
+}
     </>
 
   )
